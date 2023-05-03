5e73addc345f2c095e1d46281bfc1103d3cec719
Akshay <akshaympacad@gmail.com>
2023-05-03 22:02:26 +0530

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,28 @@ import 'package:flutter/material.dart';
 import './login.dart';
 import './register.dart';
 import './profile.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: MyLogin(),
-    routes: {
-      'register': (context) => MyRegister(),
-      'login': (context) => MyLogin(),
-      'profile':(context) => MyProfile(),
-    },
-  ));
+  runApp(MyApp());
+
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyLogin(),
+      routes: {
+        'register': (context) => MyRegister(),
+        'login': (context) => MyLogin(),
+        'profile':(context) => MyProfile(),
+      },
+    );
+
+  }
 }
 
 
